bc4c0ebc0c487f2d226f8c865181cfdda79e4f3b
Crypto_Vdim <vadimrace@gmail.com>
2023-01-24 21:56:30 -0500

    created example.html to figure out styling, working on card alignment and card content

---
+++ index.js
@@ -0,0 +1,5 @@
+//PSEUDO CODE
+//1. Style it for real
+// code out the html template seen in the example call it example.html
+//2. write the enquierer questions in index.js
+//3. break down the classes/objects for the different positions
